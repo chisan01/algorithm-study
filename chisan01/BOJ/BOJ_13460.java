496883b12e1871054acbb7bc10d853f25c72d86a
Chisan Ahn <chisan2001@gmail.com>
2022-04-18 21:58:52 +0900

    refact: [백준] Gold1 구슬 탈출 2
    DFS -> BFS

--- chisan01/BOJ/BOJ_13460.java
+++ chisan01/BOJ/BOJ_13460.java
@@ -9,13 +9,13 @@ class Board {
     // left, up, right, down
     private final int[] dx = {-1, 0, 1, 0};
     private final int[] dy = {0, -1, 0, 1};
+
     private enum DIR {LEFT, UP, RIGHT, DOWN}
 
     private final char[][] board;
     private int holeX, holeY;
     private Ball initialRedBall, initialBlueBall;
     private Ball redBall, blueBall;
-    private int ret;
 
     // 공
     class Ball {
@@ -31,10 +31,6 @@ class Board {
             this.y = copy.y;
         }
 
-        public boolean equals(Ball comp) {
-            return this.x == comp.x && this.y == comp.y;
-        }
-
         public boolean isOut() {
             return this.x == holeX && this.y == holeY;
         }
@@ -46,20 +42,21 @@ class Board {
                 int nextX = this.x + dx[dir.ordinal()];
                 char nextItem = board[nextY][nextX];
 
-                if (nextItem == 'O') { // 구멍에 도착한 경우 구멍으로 이동
+                // 구멍에 도착한 경우 구멍으로 이동
+                if (nextItem == 'O') {
                     this.x = nextX;
                     this.y = nextY;
                     break;
                 }
 
                 // 다른 공이나 벽이랑 부딪치는 경우 바로 앞 칸에서 stop
-                if(this == redBall) {
+                if (this == redBall) {
                     if (nextY == blueBall.y && nextX == blueBall.x) break;
                 }
-                if(this == blueBall) {
+                if (this == blueBall) {
                     if (nextY == redBall.y && nextX == redBall.x) break;
                 }
-                if(nextItem == '#') break;
+                if (nextItem == '#') break;
 
                 // 일반적으로 직진하는 경우
                 this.x = nextX;
@@ -73,6 +70,18 @@ class Board {
         }
     }
 
+    class Balls {
+        public Ball redBall;
+        public Ball blueBall;
+        public int time;
+
+        public Balls(Ball redBall, Ball blueBall, int time) {
+            this.redBall = new Ball(redBall);
+            this.blueBall = new Ball(blueBall);
+            this.time = time;
+        }
+    }
+
     public Board(String[] board) {
         this.board = new char[board.length][board[0].length()];
         for (int y = 0; y < board.length; y++) {
@@ -83,11 +92,11 @@ class Board {
                         initialBlueBall = new Ball(x, y);
                         this.board[y][x] = '.';
                         break;
-                    case 'R' :
+                    case 'R':
                         initialRedBall = new Ball(x, y);
                         this.board[y][x] = '.';
                         break;
-                    case 'O' :
+                    case 'O':
                         this.holeY = y;
                         this.holeX = x;
                         break;
@@ -101,45 +110,16 @@ class Board {
     // 이전에 움직였던 방향의 반대 방향으로 움직일 필요는 없다
     // 빨간 공이랑 파란 공이랑 같은 줄에 있는 경우, 더 옆에 있는 공부터 먼저 움직여야 한다.
     public void tilt(DIR dir) {
-        switch(dir.ordinal()) {
-            case 0: // LEFT, x값 감소
-                if (redBall.y == blueBall.y && redBall.x < blueBall.x) {
-                    redBall.go(DIR.LEFT);
-                    blueBall.go(DIR.LEFT);
-                } else {
-                    blueBall.go(DIR.LEFT);
-                    redBall.go(DIR.LEFT);
-                }
-                break;
-            case 1: // UP, y값 감소
-                if (redBall.x == blueBall.x && redBall.y < blueBall.y) {
-                    redBall.go(DIR.UP);
-                    blueBall.go(DIR.UP);
-                } else {
-                    blueBall.go(DIR.UP);
-                    redBall.go(DIR.UP);
-                }
-                break;
-            case 2: // RIGHT, x값 증가
-                if (redBall.y == blueBall.y && redBall.x > blueBall.x) {
-                    redBall.go(DIR.RIGHT);
-                    blueBall.go(DIR.RIGHT);
-                } else {
-                    blueBall.go(DIR.RIGHT);
-                    redBall.go(DIR.RIGHT);
-                }
-                break;
-            case 3: // DOWN, y값 증가
-                if (redBall.x == blueBall.x && redBall.y > blueBall.y) {
-                    redBall.go(DIR.DOWN);
-                    blueBall.go(DIR.DOWN);
-                } else {
-                    blueBall.go(DIR.DOWN);
-                    redBall.go(DIR.DOWN);
-                }
-                break;
-            default:
-                break;
+        if ((dir == DIR.LEFT && redBall.y == blueBall.y && redBall.x < blueBall.x) ||
+                (dir == DIR.UP && redBall.x == blueBall.x && redBall.y < blueBall.y) ||
+                (dir == DIR.RIGHT && redBall.y == blueBall.y && redBall.x > blueBall.x) ||
+                (dir == DIR.DOWN && redBall.x == blueBall.x && redBall.y > blueBall.y)
+        ) {
+            redBall.go(dir);
+            blueBall.go(dir);
+        } else {
+            blueBall.go(dir);
+            redBall.go(dir);
         }
     }
 
@@ -147,49 +127,45 @@ class Board {
     public void printBoard() {
         for (int y = 0; y < board.length; y++) {
             for (int x = 0; x < board[0].length; x++) {
-                if(redBall.x == x && redBall.y == y) System.out.print('R');
-                else if(blueBall.x == x && blueBall.y == y) System.out.print('B');
+                if (redBall.x == x && redBall.y == y) System.out.print('R');
+                else if (blueBall.x == x && blueBall.y == y) System.out.print('B');
                 else System.out.print(board[y][x]);
             }
             System.out.println();
         }
     }
 
-    public void play(int time) {
-        if (time > 10) {
-            return;
-        }
-
-        if (blueBall.isOut()) {
-            return;
-        } else if (redBall.isOut()) {
-            ret = Math.min(ret, time);
-            return;
-        }
-
-        Ball prevRedBall = new Ball(redBall);
-        Ball prevBlueBall = new Ball(blueBall);
-
-        for(DIR dir : DIR.values()) {
-            tilt(dir);
-
-//            System.out.println((time + 1) + "time " + dir.name());
-//            printBoard();
-//            System.out.println();
-
-            play(time + 1);
-            redBall.restorePosition(prevRedBall);
-            blueBall.restorePosition(prevBlueBall);
-        }
-    }
-
     public int minMoveToOutOnlyRedBall() {
-        redBall = new Ball(initialRedBall);
-        blueBall = new Ball(initialBlueBall);
-        ret = INF;
+        Queue<Balls> Q = new LinkedList<>();
+        Q.add(new Balls(initialRedBall, initialBlueBall, 0));
+        while (!Q.isEmpty()) {
+            int curTime = Q.peek().time;
+            if (curTime > 10) break;
+            redBall = Q.peek().redBall;
+            blueBall = Q.peek().blueBall;
+            Q.poll();
 
-        play(0);
-        return ret == INF ? -1 : ret;
+            if (blueBall.isOut()) continue;
+            else if (redBall.isOut()) {
+                return curTime;
+            }
+
+            Ball prevRedBall = new Ball(redBall);
+            Ball prevBlueBall = new Ball(blueBall);
+
+            for (DIR dir : DIR.values()) {
+                tilt(dir);
+
+//                System.out.println((curTime + 1) + "time " + dir.name());
+//                printBoard();
+//                System.out.println();
+
+                Q.add(new Balls(redBall, blueBall, curTime + 1));
+                redBall.restorePosition(prevRedBall);
+                blueBall.restorePosition(prevBlueBall);
+            }
+        }
+        return -1;
     }
 }
 
